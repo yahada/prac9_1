64e956d6517c5830bf023bd6ceeca1cbfcbff2ac
yahada <niadzy14@gmail.com>
2025-11-10 17:14:36 +0300

    prog_3

---
+++ prog_3.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstddef>
+
+
+int * copy (const int * v, size_t k, int * w) {
+  for (size_t i = 0; i < k; ++i) {
+    w[i] = v[i];
+  }
+  return w+k;
+}
+
+int det(int * b, const int * lhs, size_t n) {
+  if (!n) {
+    return 0;
+  } else if (n == 1) {
+    return * lhs;
+  } else if (n == 2) {
+    int d = lhs[0] * lhs[3] - lhs[1] * lhs[2];
+    return d;
+  }
+
+  size_t d = 0;
+  for (size_t l = 0; l < n; ++l) {
+    int * minor = b;
+    for (size_t i = 1; i < n; ++i) {
+      const int * lhsRow = lhs + i * n;
+      int * minRow = copy(lhsRow, l, minRow);
+      lhsRow+=l+1;
+      copy(lhsRow, n - (l+1), minRow);
+    }
+    int * next = b + (n - 1) * (n - 1);
+    d+= lhs[l] * det(next, minor, n-1) * (l % 2) ? -1 : 1;
+  }
+  return d;
+
+}
+
+
+int det(const int * lhs, size_t n) {
+  size_t k = culc_minors(n);
+  int * mem = new int [k];
+  int d = det(mem, lhs, d);
+  return d;
+}
